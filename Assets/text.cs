af028560f0e8f8e1599e33118e2ebe187f72991a
Yunfei <60729645+Fatcat1015@users.noreply.github.com>
2022-01-31 13:00:02 -0500

    111

---
+++ Assets/text.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class text : MonoBehaviour
+{
+    public TMP_Text textt;
+    public string newtext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
